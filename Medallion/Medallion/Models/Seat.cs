59d6875a5fab6846673b6aaab6189b1ac1daeba7
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-24 15:04:38 +0300

    Create Seat.cs

---
+++ Medallion/Medallion/Models/Seat.cs
@@ -0,0 +1,11 @@
+﻿namespace Medallion.Models
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        public string Section { get; set; }
+        public string Number { get; set; }
+        public double Price { get; set; }
+        public Performance Performance { get; set; }   
+    }
+}
