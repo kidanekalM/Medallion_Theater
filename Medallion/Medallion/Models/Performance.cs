408670f9efc211c674129c1591585d5dde504d63
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-24 15:05:07 +0300

    Create Performance.cs

---
+++ Medallion/Medallion/Models/Performance.cs
@@ -0,0 +1,11 @@
+﻿namespace Medallion.Models
+{
+    public class Performance
+    {
+        public int Id { get; set; }
+        public Production production{ get; set; }
+        public DateTime dateTime { get; set; }
+        public string Type { get; set; }
+
+    }
+}
