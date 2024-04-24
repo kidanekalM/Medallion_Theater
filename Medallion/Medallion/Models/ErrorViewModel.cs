11cb9e406bbe6035eff020323bb78a1c63492d36
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-24 18:17:28 +0300

    additional files

---
+++ Medallion/Medallion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medallion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
