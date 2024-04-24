03b87955912b1032c9a071d0c76f4df5f0dd6a57
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-24 15:05:02 +0300

    Create Production.cs

---
+++ Medallion/Medallion/Models/Production.cs
@@ -0,0 +1,9 @@
+﻿namespace Medallion.Models
+{
+    public class Production
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
