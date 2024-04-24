e0fd36c391672cede7e1a4ace3ec2ce02c03f621
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-24 15:04:57 +0300

    Create Report.cs

---
+++ Medallion/Medallion/Models/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace Medallion.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Data { get; set; }
+    }
+}
