c43cd366b56cb8d5a256f53ee676393a0c8b783f
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-25 10:32:31 +0300

    Update reserve.js

--- Medallion/Medallion/Views/Reservation/reserve.js
+++ Medallion/Medallion/Views/Reservation/reserve.js
@@ -5,29 +5,29 @@ document.getElementById('diagram').style.display = "none"
 let seats = document.getElementsByClassName('seat')
 for(i=0;i<seats.length;i++){
     seats[i].addEventListener('click',(e)=>{
-        console.log(e.target )
+        // console.log(e.target )
         if(e.target.style.backgroundColor != "grey"){
             if(e.target.parentElement.className.includes("box") ){
                 document.getElementById('price').value = "85"
                 // document.getElementById("txt_seat").
-                let boxElements = document.getElementsByClassName('box')[0].children;
-                for(j=0;j<=boxElements.length;j++){
-                    if(boxElements[j]==e.target){
-                        document.getElementById("txt_seat").value="X"+j
+                let boxElements1 = document.getElementsByClassName('box')[0].children;
+                let boxElements2 = document.getElementsByClassName('box')[1].children;
+                
+                for(j=0;j<=boxElements1.length;j++){
+                    if(boxElements1[j]==e.target){
+                        document.getElementById("txt_seat").value="X"+(j+1)
                     }
-                    console.log(boxElements[j])
-                    let once = true
-                    if((j+1==boxElements.length)&&(once) ){
-                        once=false
-                        boxElements = document.getElementsByClassName('box')[1].children;
-                        j=-1
+                    if(boxElements2[j]==e.target){
+                        document.getElementById("txt_seat").value="X"+(j+8+1)
                     }
                 }
 
-                console.log(boxElements)
             }
             else if(e.target.parentElement.parentElement.className == "orchestra" ){
                 document.getElementById('price').value = "65"
+                if(e.target.parentElement.className =="a"){
+
+                }
                 
             }
             else if(e.target.parentElement.parentElement.className == "mezzanine" ){
@@ -35,6 +35,15 @@ for(i=0;i<seats.length;i++){
             }
             else if(e.target.parentElement.parentElement.className == "balcony" ){
                 document.getElementById('price').value = "40"
+            }
+            if(!(e.target.parentElement.className.includes("box"))){
+                let  rowElements = document.getElementsByClassName(e.target.parentElement.className)[0].children
+                console.log(rowElements)
+                for(j=0;j<rowElements.length;j++){
+                    if(rowElements[j]==e.target){
+                        document.getElementById("txt_seat").value=(e.target.parentElement.className.toUpperCase())+(j+1)
+                    }   
+                }
             }
         }
     })
