b037f5bd273e7007085b6dcc80e20262c8baced2
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-25 19:49:07 +0300

    Create PatronController.cs

---
+++ Medallion/Medallion/Controllers/PatronController.cs
@@ -0,0 +1,29 @@
+﻿using Medallion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medallion.Controllers
+{
+    public class PatronController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Patron patron)
+        {
+            return View();
+        }
+
+
+    }
+}
