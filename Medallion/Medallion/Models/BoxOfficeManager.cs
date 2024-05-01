fd555e05fd3fcc06ac42032e31c260a7dc0ec920
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-05-01 16:53:40 +0300

    meet the req of the ppt

---
+++ Medallion/Medallion/Models/BoxOfficeManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Medallion.Models
+{
+    public class BoxOfficeManager:Person
+    {
+        public string Id { get; set; }
+    }
+}
--- Medallion/Medallion/Models/Patron.cs
+++ Medallion/Medallion/Models/Patron.cs
@@ -2,16 +2,9 @@
 
 namespace Medallion.Models
 {
-    public class Patron
+    public class Patron : Person
     {
         public string PatronId { get; set; }
-        public string FirstName {  get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string ZipCode { get; set; }
-        public string CellPhone { get; set; }
-        public string Email {  get; set; }
+
     }
 }
